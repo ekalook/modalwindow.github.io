7d66e63f414424815305bce2794238eeb55a584b
eka kala <107412612+ekalook@users.noreply.github.com>
2023-07-20 22:44:28 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("mybtn");
+const modal = document.getElementById("mymodal");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
